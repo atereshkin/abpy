ab3f6f431c5edd9d4a574e71eb63e5271dc32b79
Alex Tereshkin <atereshkin@y-node.com>
2013-04-14 11:55:02 +0300

    Proper (though incomplete) rule matching

--- ab.py
+++ ab.py
@@ -7,27 +7,62 @@ RE_TOK = re.compile('\W')
 index = {}
 
 
+MAP_RE = (('\|\|','(//|\.)'),
+          ('\^', r'[/\\:+!@#\$^\^&\*\(\)\|]'))
+
+class RuleSyntaxError(Exception):
+    pass
+
+class Rule(object):
+    def __init__(self, rule_str):
+        self.rule_str = rule_str.strip()
+        if '$' in rule_str:
+            try:
+                self.pattern, self.optstring = rule_str.split('$')
+            except ValueError:
+                raise RuleSyntaxError()
+        else:
+            self.pattern = self.rule_str
+        self.regex = self._to_regex()
+
+    def get_tokens(self):
+        return RE_TOK.split(self.pattern)
+
+    def match(self, url):
+        return self.regex.search(url)
+
+    def _to_regex(self):
+        re_str = re.escape(self.pattern)
+        for m in MAP_RE:
+            re_str = re_str.replace(*m)
+        return re.compile(re_str)
+    
+    def __unicode__(self):
+        return self.rule_str
 
 for rul in f.xreadlines():
-    tokens = RE_TOK.split(rul)
-    for tok in tokens:
-        if len(tok) > 3:
+    if rul.startswith('##'):
+        continue
+    try:
+        rule = Rule(rul)
+    except RuleSyntaxError:
+        print 'syntax error in ', rul
+    for tok in rule.get_tokens():
+        if len(tok) > 2:
             if tok not in index:
                 index[tok] = []
-            index[tok].append(rul.strip())
+            index[tok].append(rule)
 
 def match(url):
     tokens = RE_TOK.split(url)
     for tok in tokens:
-        if len(tok) > 3:
+        if len(tok) > 2:
             if tok in index:
-                for rul in index[tok]:
-                    if rule_match(rul, url):
-                        print rul
-
-def rule_match(rul, url):
-    return rul in url
+                for rule in index[tok]:
+                    if rule.match(url):
+                        print unicode(rule)
 
 
 if __name__ == '__main__':
+    print 'start matching'
     match(sys.argv[1])
