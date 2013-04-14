c32763e961640aadf43e755ce42a3f01679eee83
Alex Tereshkin <atereshkin@y-node.com>
2013-04-14 12:58:40 +0300

    Element type options support

--- ab.py
+++ ab.py
@@ -8,10 +8,23 @@ index = {}
 
 
 MAP_RE = (('\|\|','(//|\.)'),
-          ('\^', r'[/\\:+!@#\$^\^&\*\(\)\|]'))
+          ('\^', r'[/\\:+!@#\$^\^&\*\(\)\|]'),
+          ('\*', r'.*'))
 
 class RuleSyntaxError(Exception):
     pass
+
+TYPE_OPTS = (('script', 'external scripts loaded via HTML script tag'),
+             ('image', 'regular images, typically loaded via HTML img tag'),
+             ('stylesheet', 'external CSS stylesheet files'),
+             ('object', 'content handled by browser plugins, e.g. Flash or Java'),
+             ('xmlhttprequest', 'requests started by the XMLHttpRequest object'),
+             ('object-subrequest', 'requests started plugins like Flash'),
+             ('subdocument', 'embedded pages, usually included via HTML frames'),
+             ('document', 'the page itself (only exception rules can be applied to the page)'),
+             ('elemhide', 'for exception rules only, similar to document but only disables element hiding rules on the page rather than all filter rules (Adblock Plus 1.2 and higher required)'),
+             ('other', 'types of requests not covered in the list above'))
+TYPE_OPT_IDS = [x[0] for x in TYPE_OPTS]
 
 class Rule(object):
     def __init__(self, rule_str):
@@ -23,12 +36,28 @@ class Rule(object):
                 raise RuleSyntaxError()
         else:
             self.pattern = self.rule_str
+            self.optstring = ''
         self.regex = self._to_regex()
+        opts = self.optstring.split(',')
+        self.excluded_elements = []
+        self.matched_elements = []
+        for o in opts:
+            if o.startswith('~') and o[1:] in TYPE_OPT_IDS:
+                self.excluded_elements.append(o)
+            elif o in TYPE_OPT_IDS:
+                self.matched_elements.append(o)
+        if self.matched_elements == []:
+            self.matched_elements = TYPE_OPT_IDS
 
     def get_tokens(self):
         return RE_TOK.split(self.pattern)
 
-    def match(self, url):
+    def match(self, url, elementtype=None):
+        if elementtype:
+            if elementtype in self.excluded_elements or\
+                    (elementtype not in self.matched_elements and\
+                         'other' not in self.matched_elements):
+                return False
         return self.regex.search(url)
 
     def _to_regex(self):
@@ -41,7 +70,9 @@ class Rule(object):
         return self.rule_str
 
 for rul in f.xreadlines():
-    if rul.startswith('##'):
+    if rul.startswith('!'): # Comment 
+        continue 
+    if '##' in rul: # HTML rule
         continue
     try:
         rule = Rule(rul)
